5e44b30048582ec5a68a7415a607f795b40c4474
SmartSteps <smart.stepsco@gmail.com>
2017-05-24 15:05:03 +0430

    Fix

--- csharp-fix/Program.cs
+++ csharp-fix/Program.cs
@@ -13,15 +13,15 @@ namespace csharp_fix
         {
             try
             {
-                new CoinApi("73034021-0EBC-493D-8A00-E0F138111F41").twitter_last_data(5);
-                //QuickFix.SessionSettings settings = new QuickFix.SessionSettings("config_ssl.cfg");
-                //MarketDataApp application = new MarketDataApp();
-                //QuickFix.IMessageStoreFactory storeFactory = new QuickFix.FileStoreFactory(settings);
-                //QuickFix.ILogFactory logFactory = new QuickFix.ScreenLogFactory(settings);
-                //QuickFix.Transport.SocketInitiator initiator = new QuickFix.Transport.SocketInitiator(application, storeFactory, settings, logFactory);
-                //initiator.Start();
 
-                //System.Threading.Thread.Sleep(TimeSpan.MaxValue);
+                QuickFix.SessionSettings settings = new QuickFix.SessionSettings("config_ssl.cfg");
+                MarketDataApp application = new MarketDataApp();
+                QuickFix.IMessageStoreFactory storeFactory = new QuickFix.FileStoreFactory(settings);
+                QuickFix.ILogFactory logFactory = new QuickFix.ScreenLogFactory(settings);
+                QuickFix.Transport.SocketInitiator initiator = new QuickFix.Transport.SocketInitiator(application, storeFactory, settings, logFactory);
+                initiator.Start();
+
+                System.Threading.Thread.Sleep(TimeSpan.MaxValue);
             }
             catch (System.Exception e)
             {
---
+++ csharp-rest/csharp-rest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_rest {
+    class Program {
+        static void Main(string[] args) {
+            new CoinApi("73034021-0EBC-493D-8A00-E0F138111F41").twitter_last_data(5);
+
+        }
+    }
+}
